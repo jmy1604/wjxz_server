c13f51dc9dfb077ea1de18938c3222a34f0594c6
jmy1604 <jmy1604@hotmail.com>
2018-03-02 15:58:49 +0800

    add tools

---
+++ tools/sh_script/up_data_restart_all_server.sh
@@ -0,0 +1,20 @@
+set -x
+cd ../game_data
+svn up
+cd ../bin
+
+sh ./kill_all_server.sh
+
+sleep 5s
+
+nohup `pwd`/center_server &
+sleep 1s
+nohup `pwd`/rpc_server &
+sleep 1s
+nohup `pwd`/hall_server &
+sleep 1s 
+nohup `pwd`/hall_server -f `pwd`/../conf/hall_server2.cfg &
+sleep 1s
+nohup `pwd`/login_server &
+
+
