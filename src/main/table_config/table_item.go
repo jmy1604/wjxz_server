24055846d12d80bdd723dc504c1f08c14ac6e075
huoshan017 <huoshan017@hotmail.com>
2018-04-18 10:21:44 +0800

    mm

--- src/main/table_config/table_item.go
+++ src/main/table_config/table_item.go
@@ -35,7 +35,7 @@ func (this *ItemTableMgr) Init() bool {
 }
 
 func (this *ItemTableMgr) Load() bool {
-	data, err := ioutil.ReadFile("../game_data/item.xml")
+	data, err := ioutil.ReadFile("../game_data/Item.xml")
 	if nil != err {
 		log.Error("ItemTableMgr read file err[%s] !", err.Error())
 		return false
