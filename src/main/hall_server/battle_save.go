82d473c8510d78925bd02dd8e0246ef356aeb9b9
huoshan017 <huoshan017@hotmail.com>
2018-05-29 10:49:43 +0800

    mm

---
+++ src/main/hall_server/battle_save.go
@@ -0,0 +1,11 @@
+package main
+
+type BattleSaveManager struct {
+	saves *dbBattleSaveTable
+}
+
+func (this *BattleSaveManager) Init() {
+	this.saves = dbc.BattleSaves
+}
+
+func (this *BattleSaveManager) SaveNew(attacker_id, defenser_id int32, data []byte)
